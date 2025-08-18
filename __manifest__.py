2fb8086aaaef1b3b76d619322ef8babfcb13793a
rocketgithub <rodrigo@aquih.com>
2025-08-18 11:49:45 -0600

    Faltaba comillas

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'FEL Megaprint',
-    'version': '1.3,
+    'version': '1.3',
     'category': 'Custom',
     'description': """ Integración con factura electrónica de Megaprint """,
     'author': 'Rodrigo Fernandez',
